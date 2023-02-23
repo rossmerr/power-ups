c1f51c7929d07d6ae015f808d774dffa4723e35b
Ross Merrigan <email@rossmerrigan.com>
2023-02-23 20:58:55 +0100

    Update copy.js

--- js/copy.js
+++ js/copy.js
@@ -15,10 +15,10 @@ document.getElementById("save").addEventListener("click", function () {
 });
 
 t.render(function () {
-  t.card("description")
-    .get("description")
-    .then(function (description) {
-      console.log(description);
+  t.card("desc")
+    .get("desc")
+    .filter(function (attachment) {
+      console.log(attachment);
     })
     .then(function () {
       return t.sizeTo("#description");
