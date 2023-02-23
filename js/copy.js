1101c7c48db92b7f4e4254fca61e1f4e3fce3e21
Ross Merrigan <email@rossmerrigan.com>
2023-02-23 20:14:34 +0100

    Update copy.js

--- js/copy.js
+++ js/copy.js
@@ -4,26 +4,15 @@ var t = TrelloPowerUp.iframe();
 
 var descriptionSelector = document.getElementById("description");
 
-window.description.addEventListener("submit", function (event) {
-  event.preventDefault();
-  return t
-    .set("card", "shared", "description", window.estimateSize.value)
-    .then(function () {
-      t.closePopup();
-    });
-});
-
 document.getElementById("save").addEventListener("click", function () {
-  return t
-    .set("board", "shared", "description", descriptionSelector.value)
-    .then(function () {
-      t.alert({
-        message: "Saved Description!",
-        duration: 15,
-        display: "info",
-      });
-      t.closePopup();
+  return t.then(function () {
+    t.alert({
+      message: "Saved Description!",
+      duration: 15,
+      display: "info",
     });
+    t.closePopup();
+  });
 });
 
 t.render(function () {
