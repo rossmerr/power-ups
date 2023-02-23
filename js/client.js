abf3892cf3fc88b72b4342bdae0f3a16227a93af
Ross Merrigan <email@rossmerrigan.com>
2023-02-23 19:50:50 +0100

    copy popup

--- js/client.js
+++ js/client.js
@@ -3,11 +3,11 @@ TrelloPowerUp.initialize({
     return [
       {
         icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-        text: "Estimate Size",
+        text: "Copy to Keep",
         callback: function (t) {
           return t.popup({
-            title: "Estimation",
-            url: "estimate.html",
+            title: "Copy",
+            url: "copy.html",
           });
         },
       },
