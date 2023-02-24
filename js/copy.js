c845df4affa87f98982f03cb6807c12a7aa25246
Ross Merrigan <email@rossmerrigan.com>
2023-02-24 13:07:35 +0100

    testing card post

--- js/copy.js
+++ js/copy.js
@@ -1,11 +1,10 @@
 /* global TrelloPowerUp */
 
+const appKey = "af49ac90e03fe140ef7903e8a1f70b1b";
 var t = TrelloPowerUp.iframe({
-  appKey: "af49ac90e03fe140ef7903e8a1f70b1b",
+  appKey: appKey,
   appName: "Sync To Keep",
 });
-
-var descriptionSelector = document.getElementById("description");
 
 // "description-content";
 document.getElementById("save").addEventListener("click", function () {
@@ -16,12 +15,32 @@ document.getElementById("save").addEventListener("click", function () {
       let list = lines.filter((line) => line.startsWith("- "));
       list = list.map((line) => line.substring(2));
       console.log(list);
-      t.alert({
-        message: "Saved Description!",
-        duration: 15,
-        display: "info",
-      });
-      t.closePopup();
+
+      t.getRestApi()
+        .getToken()
+        .then(function (token) {
+          fetch(
+            `https://api.trello.com/1/cards?idList=63f8963af0c4c0cefac67203&key=${appKey}&token=${token}`,
+            token,
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+              },
+            }
+          )
+            .then((response) => {
+              if (response.status === 200) {
+                t.alert({
+                  message: "Saved Description!",
+                  duration: 15,
+                  display: "info",
+                });
+              }
+            })
+            .then((text) => t.closePopup())
+            .catch((err) => console.error(err));
+        });
     });
 });
 
